8083a93df49a7d5c79c4f42e2d97a8787db882bd
Michele Perrone <perrone.michele@outlook.com>
2022-06-24 12:54:57 +0200

    Update the web page It is now possible to see the plots showing the results of the metrics. The visitor can choose whether to show the audio results tables or the plots

--- WebPage/script.js
+++ WebPage/script.js
@@ -1,50 +1,86 @@
 const audio_prefix = "audio";
+const plots_prefix = "plots";
 const datasets = ['speech', 'music', 'urban'];
 const techniques = ['signals', 'no_phase', 'griffin_lim', 'dgl_biased', 'dgl_unbiased', 'melgan', 'hifigan', 'uniglow', 'squeezewave', 'univnet'];
+const techniques_names = ['Reference', 'No-phase', 'Griffin-Lim', 'DGL (biased)', 'DGL (unbiased)', 'MelGAN', 'HiFiGAN', 'UniGlow', 'SqueezeWave', 'UnivNet'];
 const n_audio_files = 5; // Change this up to 20
 var curr_player_id = 0;
 var current_dataset = 0;
 
-document.write("<h1 class='h1 text-center'>Spectrogram Player</h2>");
-document.write("<p class='p text-center'>This web page accompanies the mini-paper 'Comparing Signal Estimation Techniques from Magnitude-Only Spectrograms'.<br>Here, you can listen to the audio excerpts reconstructed by the various techniques.</p>");
+// Ge the container where we display the results (audio or plots)
+var resultsDiv = document.getElementById('results');
 
-for(idx_dataset = 0; idx_dataset < datasets.length; idx_dataset++)
+// Get the buttons
+var audioButton = document.getElementById('audioButton');
+var plotsButton = document.getElementById('plotsButton');
+
+// Add the listeners to the buttons
+audioButton.addEventListener("click", generate_audio_table);
+plotsButton.addEventListener("click", generate_plots);
+
+// Fills resultsDiv with the plots
+function generate_plots()
 {
-   current_dataset = datasets[idx_dataset];
-   document.write("<h2 class='h2 text-center'>Comparison for " + current_dataset + "</h2>");
+   var plots = "";
 
-   document.write("<table class='table table-striped table-hover'>  <thead class='font-monospace'> <tr>"); // Generate the table head
-   for(idx_technique = 0; idx_technique < techniques.length; idx_technique++)
+   for(idx_dataset = 0; idx_dataset < datasets.length; idx_dataset++) // For each dataset
    {
-      document.write("<th scope='col'>" + techniques[idx_technique] + "</th>");
+      current_dataset = datasets[idx_dataset];
+      plots += "<h2 class='h2 text-center'>Comparison for " + current_dataset + "</h2>";
+
+      // Waveform comparison
+      var current_plot = plots_prefix + "/" + current_dataset + ".svg";
+      plots += "<img class='img-fluid border border-2 p-3 m-3' src='" + current_plot + "'>";
    }
-   document.write("</tr> </thead>");
 
-   // Generate the body
-   document.write("<tbody class='table-group-divider'>");
-   for(idx_audio = 0; idx_audio < n_audio_files; idx_audio++) // For each audio file
+   resultsDiv.innerHTML = plots;
+}
+
+// Fills resultsDiv with the audio comparison table
+function generate_audio_table()
+{
+   var table = "";
+
+   for(idx_dataset = 0; idx_dataset < datasets.length; idx_dataset++)
    {
-      document.write("<tr>");
-      for(idx_technique = 0; idx_technique < techniques.length; idx_technique++) // For each technique
+      current_dataset = datasets[idx_dataset];
+      table += "<h2 class='h2 text-center'>Comparison for " + current_dataset + "</h2>";
+
+      table += "<table class='table table-striped table-hover'>  <thead class='font-monospace'> <tr>"; // Generate the table head
+      for(idx_technique = 0; idx_technique < techniques.length; idx_technique++)
       {
-         var current_audio = audio_prefix + "/" + current_dataset + "/" + techniques[idx_technique] + "_" + current_dataset + "/" + idx_audio + ".m4a";
-         console.log(current_audio);
-
-         document.write("<td>");
-
-         curr_player_id++;
-         document.write("<audio id='" + curr_player_id + "'> <source src='" + current_audio + "' type='audio/mpeg'> </audio>");
-
-         document.write("<div class='d-grid gap-2 d-md-block font-monospace'>");
-         document.write("<button class='btn btn-sm btn-success p-1 m-1'   type='button'  onclick='document.getElementById(" + curr_player_id + ").play()'>Play</button>");
-         document.write("<button class='btn btn-sm btn-secondary p-1 m-1' type='button'  onclick='document.getElementById(" + curr_player_id + ").pause()'>Stop</button>");
-         document.write("</div>");
-
-         document.write("</td>");
-
-         //document.write("<td> <audio controls> <source src='" + current_audio + "' type='audio/mpeg'> </audio>" + "</td>");
+         table += "<th scope='col'>" + techniques_names[idx_technique] + "</th>";
       }
-      document.write("</tr>");
+      table += "</tr> </thead>";
+
+      // Generate the body
+      table += "<tbody class='table-group-divider'>";
+      for(idx_audio = 0; idx_audio < n_audio_files; idx_audio++) // For each audio file
+      {
+         table += "<tr>";
+         for(idx_technique = 0; idx_technique < techniques.length; idx_technique++) // For each technique
+         {
+            var current_audio = audio_prefix + "/" + current_dataset + "/" + techniques[idx_technique] + "_" + current_dataset + "/" + idx_audio + ".m4a";
+            //console.log(current_audio);
+
+            table += "<td>";
+
+            curr_player_id++;
+            table += "<audio id='" + curr_player_id + "'> <source src='" + current_audio + "' type='audio/mpeg'> </audio>";
+
+            table += "<div class='d-grid gap-2 d-md-block font-monospace'>";
+            table += "<button class='btn btn-sm btn-success p-1 m-1'   type='button'  onclick='document.getElementById(" + curr_player_id + ").play()'>Play</button>";
+            table += "<button class='btn btn-sm btn-secondary p-1 m-1' type='button'  onclick='document.getElementById(" + curr_player_id + ").pause()'>Stop</button>";
+            table += "</div>";
+
+            table += "</td>";
+
+            //table += "<td> <audio controls> <source src='" + current_audio + "' type='audio/mpeg'> </audio>" + "</td>";
+         }
+         table += "</tr>";
+      }
+      table += "</tbody> </table>";
    }
-   document.write("</tbody> </table>");
+
+   resultsDiv.innerHTML = table;
 }
